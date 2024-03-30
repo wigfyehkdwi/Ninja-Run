8ec4f186831660523950ed833213edd94fe7d5b3
austin2011 <amoigus42069@proton.me>
2024-03-30 12:27:11 -0400

    third commit

---
+++ Assets/Scripts/Pickups.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pickups : MonoBehaviour
+{
+    public int score;   
+    public Text scoreText;
+
+    public ParticleSystem pickup;
+
+    void Start()
+    {
+      pickup.Stop();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("coin")) OnCoinTriggerEnter(other);
+    }
+
+    void OnCoinTriggerEnter (Collider other) {
+        score++;
+        scoreText.text = score.ToString();
+        Destroy(other.gameObject);
+
+        pickup.Play();
+    }
+}
